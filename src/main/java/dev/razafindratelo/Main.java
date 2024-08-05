1f205b19b15147d95dd6c2d3c62fe03e83519b6a
Abega1642 <hei.abega.2@gmail.com>
2024-08-05 09:52:10 +0300

    feat: add correction

--- src/main/java/dev/razafindratelo/Main.java
+++ src/main/java/dev/razafindratelo/Main.java
@@ -1,15 +1,6 @@
 package dev.razafindratelo;
 
-import dev.razafindratelo.streamingMusical.Artists.SoloArtist;
-import dev.razafindratelo.streamingMusical.songSet.PlayList;
-import dev.razafindratelo.streamingMusical.songs.Genre;
-import dev.razafindratelo.streamingMusical.songs.Song;
-import dev.razafindratelo.streamingMusical.user.User;
-
-import java.time.Duration;
-
 public class Main {
     public static void main(String[] args) {
-
     }
 }
--- src/main/java/dev/razafindratelo/corrections/PlayList.java
+++ src/main/java/dev/razafindratelo/corrections/PlayList.java
@@ -1,4 +1,16 @@
 package dev.razafindratelo.corrections;
 
-public class PlayList {
+import dev.razafindratelo.streamingMusical.songSet.GroupOfSongs;
+import dev.razafindratelo.streamingMusical.user.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayList extends GroupOfSongs {
+    private List<User> likes;
+
+    public PlayList(String id, String name) {
+        super(id, name);
+        this.likes = new ArrayList<>();
+    }
 }
